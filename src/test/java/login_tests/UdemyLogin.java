c4ef9f3fe22bd0244eb88ff15b3dc7391d85d510
Vedantsd19 <vedantdeokar19@gmail.com>
2021-09-27 12:09:08 +0530

    Second commit

--- src/test/java/login_tests/UdemyLogin.java
+++ src/test/java/login_tests/UdemyLogin.java
@@ -5,6 +5,7 @@ import org.testng.annotations.BeforeTest;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 
 public class UdemyLogin {
@@ -15,6 +16,7 @@ public class UdemyLogin {
 		driver.get("https://www.udemy.com/");
 		Thread.sleep(2000);
 		driver.findElement(By.xpath("//span[normalize-space()='Log in']")).click();
+		Assert.assertEquals(driver.getTitle(), "Attention Required! | Cloudfare");
 		//driver.findElement(By.name("email")).sendKeys("admin@gmail.com");
 	//	driver.findElement(By.name("password")).sendKeys("admin123");
 		//driver.findElement(By.name("submit")).click();
